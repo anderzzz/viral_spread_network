892be335de80a53baa2a3c8cbc35717814c758ce
anderzzz <ohrn.anders@gmail.com>
2020-03-08 21:08:38 -0700

    Add a few more diseases and worlds

--- templates.py
+++ templates.py
@@ -6,24 +6,64 @@ from viral_classes import Person, World, Disease
 
 # Disease constants
 DISEASES = {}
-DISEASES['Virus X_01'] = {'transmission_base_prob' : 0.50,
-                          'activate_mean' : 4.0,
+DISEASES['Virus X_01'] = {'transmission_base_prob' : 1.0/30.0,
+                          'activate_mean' : 2.0,
                           'activate_spread' : 1.0,
-                          'reveal_mean' : 8.0,
+                          'reveal_mean' : 10.0,
                           'reveal_spread' : 2.0,
                           'recover_mean' : 20.0,
                           'recover_spread' : 3.0,
-                          'succumb_mean' : 122.0,
+                          'succumb_mean' : 22.0,
                           'succumb_spread' : 3.0,
-                          'immunization_prob' : 0.00}
+                          'immunization_prob' : 1.00}
+DISEASES['Virus X_02'] = {'transmission_base_prob' : 1.0/10.0,
+                          'activate_mean' : 2.0,
+                          'activate_spread' : 1.0,
+                          'reveal_mean' : 10.0,
+                          'reveal_spread' : 2.0,
+                          'recover_mean' : 20.0,
+                          'recover_spread' : 3.0,
+                          'succumb_mean' : 22.0,
+                          'succumb_spread' : 3.0,
+                          'immunization_prob' : 1.00}
+DISEASES['Virus X_03'] = {'transmission_base_prob' : 1.0/30.0,
+                          'activate_mean' : 2.0,
+                          'activate_spread' : 1.0,
+                          'reveal_mean' : 10.0,
+                          'reveal_spread' : 2.0,
+                          'recover_mean' : 15.0,
+                          'recover_spread' : 3.0,
+                          'succumb_mean' : 22.0,
+                          'succumb_spread' : 3.0,
+                          'immunization_prob' : 1.00}
+DISEASES['Virus X_04'] = {'transmission_base_prob' : 1.0/30.0,
+                          'activate_mean' : 2.0,
+                          'activate_spread' : 1.0,
+                          'reveal_mean' : 7.0,
+                          'reveal_spread' : 2.0,
+                          'recover_mean' : 20.0,
+                          'recover_spread' : 3.0,
+                          'succumb_mean' : 22.0,
+                          'succumb_spread' : 3.0,
+                          'immunization_prob' : 1.00}
 
 # World constants
 WORLDS = {}
 WORLDS['World W_01'] = {'quarantine_policy' : None,
                         'social_graph_func' : 'population_well_mixed',
-                        'social_graph_kwargs' : {'n_people' : 2000,
+                        'social_graph_kwargs' : {'n_people' : 4000,
                                                  'n_infect_init' : 1,
-                                                 'n_avg_meet' : 50}}
+                                                 'n_avg_meet' : 60}}
+WORLDS['World W_02'] = {'quarantine_policy' : 'revealed',
+                        'social_graph_func' : 'population_well_mixed',
+                        'social_graph_kwargs' : {'n_people' : 4000,
+                                                 'n_infect_init' : 1,
+                                                 'n_avg_meet' : 60}}
+WORLDS['World W_03'] = {'quarantine_policy' : 'revealed',
+                        'social_graph_func' : 'population_well_mixed',
+                        'social_graph_kwargs' : {'n_people' : 4000,
+                                                 'n_infect_init' : 1,
+                                                 'n_avg_meet' : 30}}
 
 # Social network creation methods
 def population_well_mixed(n_people, n_infect_init, n_avg_meet):
@@ -78,8 +118,17 @@ def simulation(disease_name, world_name, n_days_max, report_interval, out_file_n
         if the_world.is_disease_free():
             break
 
-    print (viral_disease.dummy_1, viral_disease.dummy_2, viral_disease.dummy_4, viral_disease.dummy_3)
-
 if __name__ == '__main__':
 
-    simulation('Virus X_01', 'World W_01', 50, 1, 'test_run')
+    simulation('Virus X_01', 'World W_01', 100, 1, 'test_run_1_1')
+    simulation('Virus X_02', 'World W_01', 100, 1, 'test_run_2_1')
+    simulation('Virus X_03', 'World W_01', 100, 1, 'test_run_3_1')
+    simulation('Virus X_04', 'World W_01', 100, 1, 'test_run_4_1')
+    simulation('Virus X_01', 'World W_02', 100, 1, 'test_run_1_2')
+    simulation('Virus X_02', 'World W_02', 100, 1, 'test_run_2_2')
+    simulation('Virus X_03', 'World W_02', 100, 1, 'test_run_3_2')
+    simulation('Virus X_04', 'World W_02', 100, 1, 'test_run_4_2')
+    simulation('Virus X_01', 'World W_03', 100, 1, 'test_run_1_3')
+    simulation('Virus X_02', 'World W_03', 100, 1, 'test_run_2_3')
+    simulation('Virus X_03', 'World W_03', 100, 1, 'test_run_3_3')
+    simulation('Virus X_04', 'World W_03', 100, 1, 'test_run_4_3')
